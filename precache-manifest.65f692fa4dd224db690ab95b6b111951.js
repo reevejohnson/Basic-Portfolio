3d792c88f7c5e285ac5fc923b459c8e95bbcf064
Reeve Johnson <reevewjohnson@gmail.com>
2018-11-17 12:37:59 -0600

    Updates

---
+++ precache-manifest.65f692fa4dd224db690ab95b6b111951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f726c315a2df23da5d",
+    "url": "/click-memory-game/static/js/runtime~main.a4f726c3.js"
+  },
+  {
+    "revision": "403ca9ddfacea9939f7c",
+    "url": "/click-memory-game/static/js/main.403ca9dd.chunk.js"
+  },
+  {
+    "revision": "84b9bca4dcbf7bc3df32",
+    "url": "/click-memory-game/static/js/1.84b9bca4.chunk.js"
+  },
+  {
+    "revision": "403ca9ddfacea9939f7c",
+    "url": "/click-memory-game/static/css/main.1fa14606.chunk.css"
+  },
+  {
+    "revision": "84b9bca4dcbf7bc3df32",
+    "url": "/click-memory-game/static/css/1.9def4634.chunk.css"
+  },
+  {
+    "revision": "9e7df98dbec293beb82fb7ed41eab799",
+    "url": "/click-memory-game/index.html"
+  }
+];
